bb1cc5c32e0d89f13143bae21399aced50c5b425
David Padilla <david@crowdint.com>
2011-09-06 21:27:24 -0500

    Ready to test

---
+++ test/test_helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'bundler/setup'
+
+require 'minitest/autorun'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+ENV["RAILS_ENV"] = "test"
+
