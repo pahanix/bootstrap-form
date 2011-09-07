5d661a92f1bc8904934c07e54857c0c8de38e388
David Padilla <david@crowdint.com>
2011-09-07 10:40:51 -0500

    General refactoring

--- lib/bootstrap-form/form_helper.rb
+++ lib/bootstrap-form/form_helper.rb
@@ -2,29 +2,19 @@ module  ActionView
   module Helpers
     module FormHelper
       def bootstrap_text_field(object_name, method, options={})
-        error_messages = options[:object].errors[method]
-        clearfix_tag = error_messages.empty? ? 'clearfix' : 'clearfix error'
-
-        content_tag(:div,
-                    label(object_name, method) +
-                    content_tag(:div,
-                        text_field(object_name, method, options) +
-                        inline_help_tag(error_messages),
-                        class: 'input'),
-                    class: clearfix_tag)
+        bootstrap_clearfix_wrap(object_name, method, options.dup, text_field(object_name, method, options))
       end
 
       def bootstrap_password_field(object_name, method, options={})
+        bootstrap_clearfix_wrap(object_name, method, options.dup, password_field(object_name, method, options))
+      end
+
+      def bootstrap_clearfix_wrap(object_name, method, options={}, content)
         error_messages = options[:object].errors[method]
         clearfix_tag = error_messages.empty? ? 'clearfix' : 'clearfix error'
-
-        content_tag(:div,
-                    label(object_name, method) +
-                    content_tag(:div,
-                        password_field(object_name, method, options) +
-                        inline_help_tag(error_messages),
-                        class: 'input'),
-                    class: clearfix_tag)
+        content_tag(:div, label(object_name, method) +
+            content_tag(:div, content + inline_help_tag(error_messages), class: 'input'),
+            class: clearfix_tag)
       end
 
       def inline_help_tag(messages)
--- test/lib/action_view/helpers/form_helper_test.rb
+++ test/lib/action_view/helpers/form_helper_test.rb
@@ -1,47 +1,44 @@
 require 'test_helper'
 
 class FormHelperTest < ActionView::TestCase
-  def test_bootstrap_text_field
+  def test_bootstrap_clearfix_wrap
     object = mock
-    errors = { :name => [] }
+    errors = {:name => []}
     stub(object).errors { errors }
     stub(object).name { 'Object Name' }
     options = { :object => object }
+    content = ::ActiveSupport::SafeBuffer.new('content')
+    # content = ''
 
-    expected_code = %{<div class="clearfix"><label for="post_name">Name</label><div class="input"><input id="post_name" name="post[name]" size="30" type="text" value="Object Name" /></div></div>}
-    assert_equal(expected_code, bootstrap_text_field(:post, :name, options))
+    expected_code = %{<div class="clearfix"><label for="post_name">Name</label><div class="input">content</div></div>}
+    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, options, content)
   end
 
-  def test_bootstrap_text_field_with_errors
+  def test_bootstrap_clearfix_wrap_with_errors
     object = mock
     errors = {:name => ["can't be blank"]}
     stub(object).errors { errors }
     stub(object).name { 'Object Name' }
     options = { :object => object }
+    content = ::ActiveSupport::SafeBuffer.new('content')
 
-    expected_code = %{<div class="clearfix error"><label for="post_name">Name</label><div class="input"><input id="post_name" name="post[name]" size="30" type="text" value="Object Name" /><span class="help-inline">can't be blank</span></div></div>}
-    assert_equal(expected_code, bootstrap_text_field(:post, :name, options))
+    expected_code = %{<div class="clearfix error"><label for="post_name">Name</label><div class="input">content<span class="help-inline">can't be blank</span></div></div>}
+    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, options, content)
+  end
+
+  def test_bootstrap_text_field
+    options    = { :object => mock }
+    html, text_field = mock, mock
+    mock(self).text_field(:post, :name, options) { text_field }
+    mock(self).bootstrap_clearfix_wrap(:post, :name, options.dup, text_field) { html }
+    assert_equal html, bootstrap_text_field(:post, :name, options)
   end
 
   def test_bootstrap_password_field
-    object = mock
-    errors = { :name => [] }
-    stub(object).errors { errors }
-    stub(object).name { 'Object Name' }
-    options = { :object => object }
-
-    expected_code = %{<div class="clearfix"><label for="post_name">Name</label><div class="input"><input id="post_name" name="post[name]" size="30" type="password" /></div></div>}
-    assert_equal(expected_code, bootstrap_password_field(:post, :name, options))
-  end
-
-  def test_bootstrap_password_field_with_errors
-    object = mock
-    errors = {:name => ["can't be blank"]}
-    stub(object).errors { errors }
-    stub(object).name { 'Object Name' }
-    options = { :object => object }
-
-    expected_code = %{<div class="clearfix error"><label for="post_name">Name</label><div class="input"><input id="post_name" name="post[name]" size="30" type="password" /><span class="help-inline">can't be blank</span></div></div>}
-    assert_equal(expected_code, bootstrap_password_field(:post, :name, options))
+    options    = { :object => mock }
+    html, password_field = mock, mock
+    mock(self).password_field(:post, :name, options) { password_field }
+    mock(self).bootstrap_clearfix_wrap(:post, :name, options.dup, password_field) { html }
+    assert_equal html, bootstrap_password_field(:post, :name, options)
   end
 end
