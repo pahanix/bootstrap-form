df326033ca09b8d24a8aa6b5e77988fc049e6ef6
David Padilla <david@crowdint.com>
2011-09-06 23:43:56 -0500

    Password Field

--- lib/bootstrap-form/form_helper.rb
+++ lib/bootstrap-form/form_helper.rb
@@ -14,6 +14,19 @@ module  ActionView
                     class: clearfix_tag)
       end
 
+      def bootstrap_password_field(object_name, method, options={})
+        error_messages = options[:object].errors[method]
+        clearfix_tag = error_messages.empty? ? 'clearfix' : 'clearfix error'
+
+        content_tag(:div,
+                    label(object_name, method) +
+                    content_tag(:div,
+                        password_field(object_name, method, options) +
+                        inline_help_tag(error_messages),
+                        class: 'input'),
+                    class: clearfix_tag)
+      end
+
       def inline_help_tag(messages)
         messages.empty? ? '' : content_tag(:span, messages.join(','), class: 'help-inline')
       end
@@ -24,5 +37,9 @@ end
 class ActionView::Helpers::FormBuilder #:nodoc:
   def bootstrap_text_field(method, options = {})
     @template.bootstrap_text_field(@object_name, method, objectify_options(options))
+  end
+
+  def bootstrap_password_field(method, options = {})
+    @template.bootstrap_password_field(@object_name, method, objectify_options(options))
   end
 end
--- test/lib/action_view/helpers/form_helper_test.rb
+++ test/lib/action_view/helpers/form_helper_test.rb
@@ -1,25 +1,47 @@
 require 'test_helper'
 
 class FormHelperTest < ActionView::TestCase
-  def test_bootstrap_input
+  def test_bootstrap_text_field
     object = mock
     errors = { :name => [] }
     stub(object).errors { errors }
-    mock(object).name { 'Object Name' }
+    stub(object).name { 'Object Name' }
     options = { :object => object }
 
     expected_code = %{<div class="clearfix"><label for="post_name">Name</label><div class="input"><input id="post_name" name="post[name]" size="30" type="text" value="Object Name" /></div></div>}
     assert_equal(expected_code, bootstrap_text_field(:post, :name, options))
   end
 
-  def test_bootstrap_input_with_errors
+  def test_bootstrap_text_field_with_errors
     object = mock
     errors = {:name => ["can't be blank"]}
     stub(object).errors { errors }
-    mock(object).name { 'Object Name' }
+    stub(object).name { 'Object Name' }
     options = { :object => object }
 
     expected_code = %{<div class="clearfix error"><label for="post_name">Name</label><div class="input"><input id="post_name" name="post[name]" size="30" type="text" value="Object Name" /><span class="help-inline">can't be blank</span></div></div>}
     assert_equal(expected_code, bootstrap_text_field(:post, :name, options))
+  end
+
+  def test_bootstrap_password_field
+    object = mock
+    errors = { :name => [] }
+    stub(object).errors { errors }
+    stub(object).name { 'Object Name' }
+    options = { :object => object }
+
+    expected_code = %{<div class="clearfix"><label for="post_name">Name</label><div class="input"><input id="post_name" name="post[name]" size="30" type="password" /></div></div>}
+    assert_equal(expected_code, bootstrap_password_field(:post, :name, options))
+  end
+
+  def test_bootstrap_password_field_with_errors
+    object = mock
+    errors = {:name => ["can't be blank"]}
+    stub(object).errors { errors }
+    stub(object).name { 'Object Name' }
+    options = { :object => object }
+
+    expected_code = %{<div class="clearfix error"><label for="post_name">Name</label><div class="input"><input id="post_name" name="post[name]" size="30" type="password" /><span class="help-inline">can't be blank</span></div></div>}
+    assert_equal(expected_code, bootstrap_password_field(:post, :name, options))
   end
 end
