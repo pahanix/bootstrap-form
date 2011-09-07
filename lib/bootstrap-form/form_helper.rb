740d09274d96b3da49512d8044ddcd3863c7613b
David Padilla <david@crowdint.com>
2011-09-06 23:06:40 -0500

    Error handling

--- lib/bootstrap-form/form_helper.rb
+++ lib/bootstrap-form/form_helper.rb
@@ -9,12 +9,31 @@ module  ActionView
                         class: 'input'),
                     class: 'clearfix')
       end
+
+      def bootstrap_text_field_with_errors(object_name, method, options={})
+        object = options[:object]
+        content_tag(:div,
+                    label(object_name, method) +
+                    content_tag(:div,
+                        text_field(object_name, method, options) +
+                        content_tag(:span, object.errors[method].join(','),
+                            class: 'help-inline'),
+                        class: 'input'),
+                    class: 'clearfix error')
+      end
     end
   end
 end
 
 class ActionView::Helpers::FormBuilder #:nodoc:
   def bootstrap_text_field(method, options = {})
-    @template.bootstrap_text_field(@object_name, method, objectify_options(options))
+    options = objectify_options(options)
+    object  = options[:object]
+
+    if object.errors.empty?
+      @template.bootstrap_text_field(@object_name, method, options)
+    else
+      @template.bootstrap_text_field_with_errors(@object_name, method, options)
+    end
   end
 end
