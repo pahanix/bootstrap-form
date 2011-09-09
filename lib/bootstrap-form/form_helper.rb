bb246259bebccc03248f476f177af43bcfccb289
David Padilla <david@crowdint.com>
2011-09-09 15:09:36 -0500

    Some changes to make it 1.8.7 friendly

--- lib/bootstrap-form/form_helper.rb
+++ lib/bootstrap-form/form_helper.rb
@@ -2,28 +2,28 @@ module  ActionView
   module Helpers
     module FormHelper
       def bootstrap_text_field(object_name, method, options={})
-        bootstrap_clearfix_wrap(object_name, method, options.dup, text_field(object_name, method, options))
+        bootstrap_clearfix_wrap(object_name, method, text_field(object_name, method, options.dup), options)
       end
 
       def bootstrap_password_field(object_name, method, options={})
-        bootstrap_clearfix_wrap(object_name, method, options.dup, password_field(object_name, method, options))
+        bootstrap_clearfix_wrap(object_name, method, password_field(object_name, method, options.dup), options)
       end
 
       def bootstrap_collection_select(object_name, method, collection, value_method, text_method, options = {}, html_options = {})
-        bootstrap_clearfix_wrap(object_name, method, options.dup, collection_select(object_name, method, collection, value_method, text_method, options, html_options))
+        bootstrap_clearfix_wrap(object_name, method, collection_select(object_name, method, collection, value_method, text_method, options.dup, html_options), options)
       end
 
-      def bootstrap_clearfix_wrap(object_name, method, options={}, content)
+      def bootstrap_clearfix_wrap(object_name, method, content, options={})
         error_messages = options[:object].errors[method]
         clearfix_tag = error_messages.empty? ? 'clearfix' : 'clearfix error'
         content_tag(:div, label(object_name, method) +
-            content_tag(:div, content + inline_help_tag(error_messages), class: 'input'),
-            class: clearfix_tag)
+            content_tag(:div, content + inline_help_tag(error_messages), :class => 'input'),
+            :class => clearfix_tag)
       end
 
       def inline_help_tag(messages)
         message_span = ActiveSupport::SafeBuffer.new(" #{messages.join(',')}")
-        messages.empty? ? '' : content_tag(:span, message_span, class: 'help-inline')
+        messages.empty? ? '' : content_tag(:span, message_span, :class => 'help-inline')
       end
     end
   end
--- test/lib/action_view/helpers/form_helper_test.rb
+++ test/lib/action_view/helpers/form_helper_test.rb
@@ -10,7 +10,7 @@ class FormHelperTest < ActionView::TestCase
     stub(object).name   { 'Object Name' }
 
     expected_code = %{<div class="clearfix"><label for="post_name">Name</label><div class="input">content</div></div>}
-    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, options, content)
+    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, content, options)
   end
 
   def test_bootstrap_clearfix_wrap_with_errors
@@ -22,7 +22,7 @@ class FormHelperTest < ActionView::TestCase
     stub(object).name   { 'Object Name' }
 
     expected_code = %{<div class="clearfix error"><label for="post_name">Name</label><div class="input">content<span class="help-inline"> can't be blank</span></div></div>}
-    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, options, content)
+    assert_equal expected_code, bootstrap_clearfix_wrap(:post, :name, content, options)
   end
 
   def test_bootstrap_text_field
@@ -30,7 +30,7 @@ class FormHelperTest < ActionView::TestCase
     options = { :object => mock }
 
     mock(self).text_field(:post, :name, options) { text_field }
-    mock(self).bootstrap_clearfix_wrap(:post, :name, options.dup, text_field) { html }
+    mock(self).bootstrap_clearfix_wrap(:post, :name, text_field, options.dup) { html }
     assert_equal html, bootstrap_text_field(:post, :name, options)
   end
 
@@ -39,7 +39,7 @@ class FormHelperTest < ActionView::TestCase
     options = { :object => mock }
 
     mock(self).password_field(:post, :password, options) { password_field }
-    mock(self).bootstrap_clearfix_wrap(:post, :password, options.dup, password_field) { html }
+    mock(self).bootstrap_clearfix_wrap(:post, :password, password_field, options.dup) { html }
     assert_equal html, bootstrap_password_field(:post, :password, options)
   end
 
@@ -48,7 +48,7 @@ class FormHelperTest < ActionView::TestCase
     options = { :object => mock }
 
     mock(self).collection_select(:post, :name, collection, :id, :name, options, {}) { collection_select_html }
-    mock(self).bootstrap_clearfix_wrap(:post, :name, options.dup, collection_select_html) { html }
+    mock(self).bootstrap_clearfix_wrap(:post, :name, collection_select_html, options.dup) { html }
 
     assert_equal html, bootstrap_collection_select(:post, :name, collection, :id ,:name, options)
   end
